1585236fbf064f83618acd17300a1a91f0ca2199
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-26 23:28:35 -0300

    resgatando pedidos do banco de dados  :white_check_mark:

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,7 +2,7 @@
   <div id="burger-table">
 
     <div id="burger-table-heading">
-      <div class="order-id">#:</div>
+      <div class="order-id">#</div>
       <div>Cliente <i class="fa-solid fa-user"></i></div>
       <div>Pão <i class="fa-solid fa-bread-slice"></i></div>
       <div>Carne <i class="fa-solid fa-drumstick-bite"></i></div>
@@ -11,15 +11,14 @@
     </div>
 
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão de Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
           </ul>
         </div>
         <div>
@@ -35,17 +34,54 @@
 
 <script>
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data(){
+    return{
+      burgers:null,
+      burger_id:null,
+      status:[]
+    }
+  },
+  methods:{
+    async getPedidos(){
+      const req = await fetch('http://localhost:3000/burgers')
+      const data = await req.json()
+      this.burgers = data
+      console.log(this.burgers)
+
+      //resgatar os status
+
+
+    }
+  },
+  mounted() {
+    this.getPedidos()
+  },
 }
 </script>
 
 <style lang="scss" scoped>
- 
+::-webkit-scrollbar-track {
+    background-color: #F4F4F4;
+}
+::-webkit-scrollbar {
+    width: .4rem;
+    background: #F4F4F4;
+}
+::-webkit-scrollbar-thumb {
+    background: #dad7d7;
+}
+ li{
+  list-style: none;
+ }
 #burger-table {
-  max-width: 1200px;
+  width: 1200px;
   margin: 0 auto;
   margin-top: 1.3rem;
-
+  max-height:330px;
+  overflow: scroll;
+  overflow-x: hidden;
+  
 }
 
 #burger-table-heading {
@@ -101,7 +137,7 @@ select {
 
 .delete-btn {
   background-color: $h1;
-  color: #fcba03;
+  color: $link;
   font-weight: bold;
   border: 0.12rem solid $h1;
   padding: 0.62rem;
